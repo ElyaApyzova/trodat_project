47ee32bb43780605b970ee1f0564e1ef61a30e2d
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-08-09 01:56:32 +0300

    completed

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+//menu
+
+const menuBtn = document.querySelector(".menu__btn");
+const menuMobile = document.querySelector(".header__menu-list");
+
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu__open");
+});
